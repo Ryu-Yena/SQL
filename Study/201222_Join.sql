a624a7024fbf2b24f52036e40815ad1ae19134d7
Lamant <uyti0713@gmail.com>
2020-12-22 01:44:53 +0900

    201222_Join

---
+++ Study/201222_Join.sql
@@ -0,0 +1,37 @@
+/* outer join
+join 조건을 만족하지 않는 컬럼이 없는 경우 Null을 포함하여 결과를 생성 
+└ Null이 올 수 있는 쪽 조건에 (+)를 붙인다.
+*/
+
+
+--full out join
+select em.department_id,
+       em.first_name,
+       de.department_name,
+       de.department_id
+from employees em full outer join departments de 
+on em.department_id = de.department_id;
+
+
+select em.department_id,
+       em.first_name,
+       de.department_name,
+       de.department_id
+from employees em, departments de
+where em.department_id(+) = de.department_id(+); --사용하지 않는다.
+
+--alias
+select /*employees*/em.department_id, --별명을 지어주면 무조건 별명을 사용해야함
+       em.first_name,
+       de.department_name,
+       de.department_id
+from employees em, departments de
+where em.department_id = de.department_id
+order by em.department_id desc;
+
+select *
+from employees
+
+
+select *
+from employees
